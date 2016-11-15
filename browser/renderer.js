993a4a8a3f7818e4f6c5548d078170ff5a47796a
Satoshi Watanabe <sassy.watanabe@gmail.com>
2016-11-16 00:31:25 +0900

    divide script file.

---
+++ browser/renderer.js
@@ -0,0 +1,45 @@
+const videoList = [
+  'S_ajnkUREA8',
+  'GFGN5fa3Go4',
+  '0IrUT0sXrBU'
+];
+const utime = Math.floor( new Date().getTime() / 1000 );
+const index = utime % videoList.length;
+
+const tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+const videoInfo = {
+  height: '390',
+  width: '640',
+  videoId: videoList[index],
+  playerVars : {
+    "controls" : '0'
+  },
+  events: {
+    'onReady': onPlayerReady,
+    'onStateChange': onPlayerStateChange
+  }
+};
+
+if (index === 0) {
+  videoInfo.playerVars.start = '9';
+}
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', videoInfo);
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.ENDED) {
+    var ipc = require('electron').ipcRenderer;
+    ipc.send('close');
+  }
+}
